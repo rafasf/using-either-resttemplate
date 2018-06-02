f78b38cd9e612781066ab057af26e1882a1cae44
Rafael Ferreira <rafaelsf@gmail.com>
2018-06-02 08:03:14 -0500

    Hello World

---
+++ src/main/java/com/rafasf/anapp/Application.java
@@ -0,0 +1,11 @@
+package com.rafasf.anapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
